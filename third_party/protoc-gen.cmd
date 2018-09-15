27ed1a0ed44cfb9361404fe58ce258144e0d51f7
Aleksandr Sokolovskii <github@amsokol.com>
2018-09-15 12:04:07 +0300

    added HTTP/REST annotations

--- third_party/protoc-gen.cmd
+++ third_party/protoc-gen.cmd
@@ -1,1 +1,3 @@
-protoc --proto_path=api/proto/v1 --proto_path=third_party/google/protobuf --go_out=plugins=grpc:pkg/api/v1 todo-service.proto
+protoc --proto_path=api/proto/v1 --proto_path=third_party --go_out=plugins=grpc:pkg/api/v1 todo-service.proto
+protoc --proto_path=api/proto/v1 --proto_path=third_party --grpc-gateway_out=logtostderr=true:pkg/api/v1 todo-service.proto
+protoc --proto_path=api/proto/v1 --proto_path=third_party --swagger_out=logtostderr=true:api/swagger/v1 todo-service.proto
